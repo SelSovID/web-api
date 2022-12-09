90b91dfa2d2e20d7612b4fef4aaf5023d5a040d0
Mees van Dijk <mees@mees.io>
2022-12-09 11:49:08 +0100

    Add koa-router

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import koa from "koa"
 import { createServer } from "http"
+import koaBody from "koa-body"
+import Router from "koa-router"
 
 const app = new koa()
+const router = new Router()
+
+app.use(koaBody())
 
 app.use(async ctx => {
   ctx.body = "Hello World"
