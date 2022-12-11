bbaf0d5b6697aba385aff985d655d5f81aa23666
Mees van Dijk <mees@mees.io>
2022-12-11 14:12:11 +0100

    Change request data format

--- src/routes/request.ts
+++ src/routes/request.ts
@@ -6,24 +6,20 @@ const router = new Router<{}, MyContext>()
 router.get("/", async ctx => {
   ctx.body = [
     {
-      aanvraagID: "1",
-      voornaam: "Billy",
-      achternaam: "Bob",
-      bsn: "123456789",
-      aanvraagType: "rijbewijs",
-      datum: "08/12/2022",
-      tijd: "12:55",
-      detail: "Hallo ik ben billy bob en ik wil graag rijden",
+      id: 1,
+      fromUser: {
+        email: "billybob@fmail.com",
+      },
+      date: Date.now(),
+      requestText: "Hallo ik ben billy bob en ik wil graag rijden",
     },
     {
-      aanvraagID: "2",
-      voornaam: "Bob",
-      achternaam: "BBilly",
-      bsn: "0000000000",
-      aanvraagType: "Loonstrook",
-      datum: "25/05/2022",
-      tijd: "12:57",
-      detail: "heel veel geld",
+      id: 2,
+      fromUser: {
+        email: "ShawnTheSheep@godmail.com",
+      },
+      date: Date.now(),
+      requestText: "heel veel geld",
     },
   ]
 })
