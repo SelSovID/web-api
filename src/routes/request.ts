13e7d098e53d61439acbe28768177398ca193765
Mees van Dijk <mees@mees.io>
2022-12-11 14:07:12 +0100

    Add dummy data endpoint

--- src/routes/request.ts
+++ src/routes/request.ts
@@ -4,7 +4,28 @@ import { MyContext } from "../index.js"
 const router = new Router<{}, MyContext>()
 
 router.get("/", async ctx => {
-  ctx.body = "Hello World"
+  ctx.body = [
+    {
+      aanvraagID: "1",
+      voornaam: "Billy",
+      achternaam: "Bob",
+      bsn: "123456789",
+      aanvraagType: "rijbewijs",
+      datum: "08/12/2022",
+      tijd: "12:55",
+      detail: "Hallo ik ben billy bob en ik wil graag rijden",
+    },
+    {
+      aanvraagID: "2",
+      voornaam: "Bob",
+      achternaam: "BBilly",
+      bsn: "0000000000",
+      aanvraagType: "Loonstrook",
+      datum: "25/05/2022",
+      tijd: "12:57",
+      detail: "heel veel geld",
+    },
+  ]
 })
 
 router.post("/", async ctx => {
