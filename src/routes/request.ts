1cb463b9cb0b6d300e8f1358d59f0319712faaa1
Mees van Dijk <mees@mees.io>
2022-12-11 15:58:46 +0100

    Fix request body format

--- src/routes/request.ts
+++ src/routes/request.ts
@@ -2,17 +2,26 @@ import Router from "koa-router"
 import { MyContext, MyState } from "../index.js"
 import VCRequest from "../model/VCRequest.js"
 
+type RequestDTO = {
+  id: number
+  fromUser: {
+    email: string
+  }
+  date: number
+  requestText: string
+}
+
 const router = new Router<MyState, MyContext>()
 
 router.get("/", async ctx => {
   const requests = await ctx.orm.find(VCRequest, { forUser: ctx.state.user })
 
-  ctx.body = requests.map(request => ({
+  ctx.body = requests.map<RequestDTO>(request => ({
     id: request.id,
     fromUser: {
       email: request.fromEmail,
     },
-    date: request.createdAt,
+    date: request.createdAt.getTime(),
     requestText: request.text,
   }))
   ctx.status = 200
