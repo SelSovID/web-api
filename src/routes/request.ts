b514420627c3719b2cd732b25183cdbefcea7245
Mees van Dijk <mees@mees.io>
2022-12-16 14:06:38 +0100

    Implement request specific endpoint

--- src/routes/request.ts
+++ src/routes/request.ts
@@ -11,19 +11,21 @@ type RequestDTO = {
   requestText: string
 }
 
+const mapRequestToDTO = (request: VCRequest): RequestDTO => ({
+  id: request.id,
+  fromUser: {
+    email: request.fromEmail,
+  },
+  date: request.createdAt.getTime(),
+  requestText: request.text,
+})
+
 const router = new Router<MyState, MyContext>()
 
 router.get("/", async ctx => {
   const requests = await ctx.orm.find(VCRequest, { forUser: ctx.state.user })
 
-  ctx.body = requests.map<RequestDTO>(request => ({
-    id: request.id,
-    fromUser: {
-      email: request.fromEmail,
-    },
-    date: request.createdAt.getTime(),
-    requestText: request.text,
-  }))
+  ctx.body = requests.map(mapRequestToDTO)
   ctx.status = 200
 })
 
@@ -32,7 +34,20 @@ router.post("/", async ctx => {
 })
 
 router.get("/:id", async ctx => {
-  ctx.body = "Hello World"
+  const requestId = parseInt(ctx.params.id)
+  if (!isNaN(requestId)) {
+    const request = await ctx.orm.findOne(VCRequest, { forUser: ctx.state.user, id: requestId })
+    if (request != null) {
+      ctx.body = mapRequestToDTO(request)
+      ctx.status = 200
+    } else {
+      ctx.status = 404
+      ctx.body = "Request not found"
+    }
+  } else {
+    ctx.status = 400
+    ctx.body = "Invalid request id"
+  }
 })
 
 router.put("/:id", async ctx => {
