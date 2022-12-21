6dc2697dc65b87de95b243bd8cfdfc56fe328a03
Mees van Dijk <mees@mees.io>
2022-12-21 17:50:34 +0100

    Remove hardcoded test user

--- src/seeders/DatabaseSeeder.ts
+++ src/seeders/DatabaseSeeder.ts
@@ -1,3 +1,4 @@
+import "dotenv/config"
 import type { EntityData, EntityManager } from "@mikro-orm/core"
 import { Factory, Seeder } from "@mikro-orm/seeder"
 import User from "../model/User.js"
@@ -6,13 +7,14 @@ import VCRequest from "../model/VCRequest.js"
 
 export class DatabaseSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
-    const testUser = em.create(User, await User.create("testpassword"))
-
-    new VCRequestFactory(em)
-      .each(req => {
-        req.forUser = testUser
-      })
-      .make(10)
+    if (process.env.TEST_PASSWORD != null) {
+      const testUser = em.create(User, await User.create(process.env.TEST_PASSWORD))
+      new VCRequestFactory(em)
+        .each(req => {
+          req.forUser = testUser
+        })
+        .make(10)
+    }
 
     for (let i = 0; i < 10; i++) {
       const user = em.create(User, await User.create(faker.internet.password()))
