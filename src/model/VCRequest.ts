4c44b4a85a19fb084c2a82558d992215b024b35e
Mees van Dijk <mees@mees.io>
2023-01-02 19:01:54 +0100

    Add seeder

--- src/model/VCRequest.ts
+++ src/model/VCRequest.ts
@@ -23,11 +23,12 @@ export default class VCRequest {
 
   // The references below are stirngs to avoid a circular dependency
   @OneToMany({ mappedBy: (cert: SSICert) => cert.forRequest, cascade: [Cascade.ALL] })
-  attachedVCs = new Collection<SSICert>(this)
+  attachedVCs = new Collection<SSICert, this>(this)
 
-  constructor(fromEmail: string, text: string, forUser: User | null = null) {
+  constructor(fromEmail: string, text: string, forUser: User, attachedVCs: SSICert[] = []) {
     this.fromEmail = fromEmail
     this.text = text
     this.forUser = forUser!
+    this.attachedVCs = Collection.create(this, "attachedVCs", attachedVCs, false)
   }
 }
--- src/seeders/DatabaseSeeder.ts
+++ src/seeders/DatabaseSeeder.ts
@@ -1,35 +1,45 @@
 import "dotenv/config"
-import type { EntityData, EntityManager } from "@mikro-orm/core"
-import { Factory, Seeder } from "@mikro-orm/seeder"
+import type { EntityManager } from "@mikro-orm/core"
+import crypto from "node:crypto"
+import { Seeder } from "@mikro-orm/seeder"
 import User from "../model/User.js"
-import { Faker, faker } from "@faker-js/faker"
+import { faker } from "@faker-js/faker"
 import VCRequest from "../model/VCRequest.js"
+import SSICert from "../model/SSICert.js"
+import { promisify } from "node:util"
 
 export class DatabaseSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
     if (process.env.TEST_PASSWORD != null) {
       const testUser = em.create(User, await User.create(process.env.TEST_PASSWORD))
-      new VCRequestFactory(em)
-        .each(req => {
-          req.forUser = testUser
-        })
-        .make(10)
+      const reqs = await make(createVCRequest.bind(null, testUser), 10)
+      em.persist(reqs)
     }
 
     for (let i = 0; i < 10; i++) {
       const user = em.create(User, await User.create(faker.internet.password()))
-      new VCRequestFactory(em)
-        .each(req => {
-          req.forUser = user
-        })
-        .make(10)
+      const reqs = await make(createVCRequest.bind(null, user), 10)
+      em.persist(reqs)
     }
   }
 }
 
-class VCRequestFactory extends Factory<VCRequest> {
-  model = VCRequest
-  protected definition(faker: Faker): EntityData<VCRequest> {
-    return new VCRequest(faker.internet.email(), faker.lorem.sentence())
-  }
+const make = <T>(factory: () => Promise<T> | T, amount: number = 1): Promise<T[]> =>
+  Promise.all(new Array(amount).fill(null).map(factory))
+
+async function createVCRequest(user: User): Promise<VCRequest> {
+  return new VCRequest(faker.internet.email(), faker.lorem.sentence(), user, await make(createSSICert, 3))
+}
+
+/**
+ * Creates a self-signed certificate
+ * @param request what request to attach the cert to
+ * @returns a new SSICert
+ */
+async function createSSICert(): Promise<SSICert> {
+  const { publicKey, privateKey } = await promisify(crypto.generateKeyPair)("rsa", {
+    modulusLength: 2048,
+    publicExponent: 0x10001,
+  })
+  return SSICert.create(publicKey, faker.lorem.sentence(), privateKey)
 }
