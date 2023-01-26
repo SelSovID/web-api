5568ff3699839796c5ab75e4f672049f8c43df7c
Mees van Dijk <mees@mees.io>
2023-01-26 13:20:55 +0100

    Add help text to generate-cert.ts

--- src/generate-cert.ts
+++ src/generate-cert.ts
@@ -13,6 +13,7 @@ const {
     parentCertificate: parentCertificatePath,
     parentCertificateKey: parentCertificateKeyPath,
     text,
+    help,
   },
 } = parseArgs({
   options: {
@@ -36,11 +37,42 @@ const {
       type: "string",
       short: "t",
     },
+    help: {
+      type: "boolean",
+      short: "h",
+    },
   },
 })
 
+const helpText = `
+Usage: generate-cert [options]
+
+This command can generate a new SSI certificate and optionally sign it with a
+parent certificate.
+
+If you provide a parent certificate, the new certificate will be signed with it.
+If you don't provide a parent certificate, a self-signed certificate will be
+generated.
+
+You have to provide both parentCertificate and parentCertificateKey if you want
+to sign the new certificate with a parent certificate.
+
+Options:
+
+  --certificate, -c The path to the certificate file to be generated
+  --text, -t The text to be written on the certificate
+  --privateKey, -k The path to the private key file to be generated, this is optional, if you don't provide it the private key will not be saved
+  --parentCertificate, -p The path to the parent certificate file to be used to sign the new certificate. If not provided, a self-signed certificate will be generated
+  --parentCertificateKey, -K The path to the parent certificate private key file to be used to sign the new certificate. If not provided, a self-signed certificate will be generated
+`
+
+if (help) {
+  console.log(helpText)
+  process.exit(0)
+}
+
 if (!certificatePath) {
-  throw new Error("Certificate path not provided")
+  throw new Error("Certificate path not provided. Use --help to see usage")
 }
 
 const { publicKey, privateKey } = await generateKeyPair("rsa", {
@@ -52,7 +84,9 @@ const cert = await createCert(publicKey, text ?? "SSI CERTIFICATE", privateKey)
 
 if (parentCertificatePath) {
   if (!parentCertificateKeyPath) {
-    throw new Error("A parent certificate was provided but no parent certificate key was provided")
+    throw new Error(
+      "A parent certificate was provided but no parent certificate key was provided. Use --help to see usage",
+    )
   }
   const parentCert = importCert(readFileSync(parentCertificatePath))
   const parentCertKey = createPrivateKey(readFileSync(parentCertificateKeyPath, "utf8"))
